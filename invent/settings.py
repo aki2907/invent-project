fe0776e64210d6a939ac36814769c151e1e5df78
Akanksha <aki.a2907@gmail.com>
2025-09-12 00:07:28 +0530

    Set locale to India for number formatting

--- invent/settings.py
+++ invent/settings.py
@@ -114,7 +114,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-IN'
+USE_THOUSAND_SEPARATOR = True
 
 TIME_ZONE = 'UTC'
 
